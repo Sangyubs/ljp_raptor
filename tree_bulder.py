66787c9f880c9d1c07e61064362a9c71d781525c
yubii2 <yubii02@gmail.com>
2024-11-29 10:42:24 +0900

    [ADD] 파일을  추가

---
+++ tree_bulder.py
@@ -0,0 +1,7 @@
+class TreeBuilder:
+    '''
+    The TreeBuilder class is responsible for building a hierarchical text abstraction
+    structure, known as a "tree," using summarization models and
+    embedding models.
+    '''
+    
