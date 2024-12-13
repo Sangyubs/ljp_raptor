9eb087ae4d5ad4904c11f9bf411da1c328ebc6b0
yubii2 <yubii02@gmail.com>
2024-12-13 17:15:54 +0900

    [Fix] tree load_path가 있는지 여부에 따라 분기해 작업하도록 설정정

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
+'''
+사용법 : 
+이미 tree가 있다면 load_path를 지정해주고, 
+없다면 add_documents()를 통해 문서를 추가하고 save_path를 지정해준다.
+그 후, answer_question을 통해 질문에 대한 답을 얻을 수 있다.
+'''
+# from json import load
 from retrieveraugmentation import RetrievalAugmentation
 
-RA = RetrievalAugmentation()
-save_path = "./data/tree"
-RA.add_documents()
-RA.save(save_path)
+load_path = None
+
+if load_path is None:
+    # tree가 없다면 생성 후 저장
+    RA = RetrievalAugmentation()
+    # tree 생성
+    RA.add_documents()
+    # tree 저장
+    save_path = "./data/tree"
+    RA.save(save_path)
+else:
+    # tree가 있다면 로드
+    RA = RetrievalAugmentation(tree = load_path)
+ 
 question = "다른 사람이 카페에 놓아둔 지갑을 가져갔다. 해당하는 형법 법조항은?"
 answer = RA.answer_question(question)
