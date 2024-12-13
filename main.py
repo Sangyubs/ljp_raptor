6435f5f00fdc3ed161ee559e8436586dff4461d3
yubii2 <yubii02@gmail.com>
2024-12-13 17:09:14 +0900

    [Add] main에 save 함수 추가가

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ from retrieveraugmentation import RetrievalAugmentation
 RA = RetrievalAugmentation()
 save_path = "./data/tree"
 RA.add_documents()
+RA.save(save_path)
 question = "다른 사람이 카페에 놓아둔 지갑을 가져갔다. 해당하는 형법 법조항은?"
 answer = RA.answer_question(question)
