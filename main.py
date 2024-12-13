76f49e453c821f96bdd7d86a2f5b8ca12145c985
yubii2 <yubii02@gmail.com>
2024-12-13 17:07:22 +0900

    [Add] save_path, save 실행을 추가함함

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from retrieveraugmentation import RetrievalAugmentation
 
 RA = RetrievalAugmentation()
+save_path = "./data/tree"
 RA.add_documents()
-question = "다른 사람이 카페에 놓아둔 지갑을 가져갔다."
+question = "다른 사람이 카페에 놓아둔 지갑을 가져갔다. 해당하는 형법 법조항은?"
 answer = RA.answer_question(question)
